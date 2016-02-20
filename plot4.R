9dccbb692ab7d352936579261359b1346c28f752
greenleaf4ever <luye.chang@gmail.com>
2016-02-20 15:23:46 -0800

    Added files via upload

---
+++ plot4.R
@@ -0,0 +1,48 @@
+
+## Get dataset
+setwd("C:/Users/schang/Desktop/Luye Dropbox/Dropbox/Coursera/Exploratory Data Analysis/Week 1")
+data <- read.table("C:/Users/schang/Desktop/Luye Dropbox/Dropbox/Coursera/Exploratory Data Analysis/Week 1/household_power_consumption.txt",
+                   header = TRUE, sep = ";", colClasses = c("character", "character", rep("numeric",7)), na = "?")
+attach(data)
+
+## subset
+subset <- Date == "1/2/2007" | Date == "2/2/2007"
+subData <- data[subset, ]
+attach(subData)
+x <- paste(Date, Time)
+subData$DateTime <- strptime(x, "%d/%m/%Y %H:%M:%S")
+rownames(subData) <- 1:nrow(subData)
+attach(subData)
+
+##create 4 graphs
+png(filename = "plot4.png", 
+    width = 480, height = 480,
+    units = "px", bg = "transparent")
+par(mfrow = c(2, 2))
+## Top-left
+plot(DateTime, Global_active_power, 
+     type = "l",
+     xlab = "", ylab = "Global Active Power")
+## Top-right
+plot(DateTime, Voltage,
+     type = "l",
+     xlab = "datetime", ylab = "Voltage")
+## Bottom-left
+plot(DateTime, Sub_metering_1, 
+     type = "l",
+     col = "black",
+     xlab = "", ylab = "Energy sub metering")
+lines(DateTime, Sub_metering_2, col = "red")
+lines(DateTime, Sub_metering_3, col = "blue")
+# Remove the border of legend here.
+legend("topright", 
+       bty = "n",
+       col = c("black", "red", "blue"),
+       c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+       lwd = 1)
+## Bottom-right
+plot(DateTime, Global_reactive_power, 
+     type = "l",
+     col = "black",
+     xlab = "datetime", ylab = colnames(newData)[4])
+dev.off()
